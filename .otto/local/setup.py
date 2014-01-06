9482ed5eee0880b010cec00a266d884b27e0b5b1
Nic Roland <nicroland9@gmail.com>
2014-01-06 17:22:07 +0000

    Some big improvements to `otto setup`.

--- .otto/local/setup.py
+++ .otto/local/setup.py
@@ -2,6 +2,21 @@ import otto.utils as otto
 import os
 import os.path
 
+SETUP_CODE = {
+        None: '',
+        'Ubuntu': """sudo echo "deb http://repository.spotify.com stable non-free" >> /etc/apt/sources.list
+                     wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+                     sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+                     sudo echo "deb http://debian.sur5r.net/i3/ $(lsb_release -c -s) universe" >> /etc/apt/sources.list
+                     sudo apt-get update
+                     sudo apt-get --allow-unauthenticated install sur5r-keyring
+                     sudo apt-get update""",
+        'CentOS': '',
+        }
+
+SUPPORTED_PLATFORMS = SETUP_CODE.keys()
+SUPPORTED_PLATFORMS.remove(None)
+
 PPAS = [
         'ppa:ubuntu-x-swat/x-updates',
         ]
@@ -13,18 +28,21 @@ PACKAGES = {
             'screen',
             'exuberant-ctags',
             'terminator',
-            'python',
             'pandoc',
             'i3',
             'feh',
             ],
-        'CentOS5': [],
+        'CentOS': [],
+        'python': [
+            #'https://github.com/Lokaltog/powerline/tarball/develop',
+            ],
         }
-SUPPORTED_PLATFORMS = PACKAGES.keys()
 
-PIP_PACKAGES = [
-        #'https://github.com/Lokaltog/powerline/tarball/develop',
-        ]
+PACKAGE_INSTALL_CMD = {
+        'Ubuntu': "sudo apt-get install -y %s",
+        'CentOS': "sudo yum install -y %s",
+        'python': "sudo pip install %s",
+        }
 
 SYMLINKS = [
         ('dotvimrc', '.vimrc'),
@@ -36,6 +54,22 @@ CP_FILES = [
         ('dotgitconfig', '.gitconfig'),
         ('monofur.otf', '/usr/share/fonts/truetype/'),
         ]
+
+def _run_script(script):
+    res = ''
+    if script:
+        lines = script.splitlines()
+        for cmd in lines:
+            res += otto.shell(cmd.strip())
+
+    return res
+
+def _packages(platform):
+    """Install user packages"""
+    if PACKAGES[platform]:
+        otto.info("Installing %s packages..." % platform)
+        to_install = ' '.join(PACKAGES[platform])
+        otto.shell(PACKAGE_INSTALL_CMD[platform] % to_install)
 
 class Setup(otto.OttoCmd):
     def run(self, gui=True):
@@ -54,34 +88,21 @@ class Setup(otto.OttoCmd):
         # Figure out some important details
         dotfiles_path = os.path.relpath(os.getcwd(), os.path.expanduser('~'))
 
+        print
+
+        # General setup
+        _run_script(SETUP_CODE[None])
+        _run_script(SETUP_CODE[platform.result])
+
         # Get all PPAs setup
-        if platform.result == 'Ubuntu':
-            otto.info("Adding various non-Ubuntu PPAs the hardway...")
-            otto.shell('sudo echo "deb http://repository.spotify.com stable non-free" >> /etc/apt/sources.list')
-            otto.shell('wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -')
-            otto.shell("""sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'""")
-            otto.shell('sudo echo "deb http://debian.sur5r.net/i3/ $(lsb_release -c -s) universe" >> /etc/apt/sources.list')
-            otto.shell("sudo apt-get update")
-            otto.shell("sudo apt-get --allow-unauthenticated install sur5r-keyring")
+        if platform.result == 'Ubuntu' and PPAS:
+            otto.info("Adding regular PPAs...")
+            for ppa in PPAS:
+                otto.shell("sudo add-apt-repository %s" % ppa)
             otto.shell("sudo apt-get update")
 
-            if PPAS:
-                otto.info("Adding regular PPAs...")
-                for ppa in PPAS:
-                    otto.shell("sudo add-apt-repository %s" % ppa)
-                otto.shell("sudo apt-get update")
-
-        # Install user packages
-        if PACKAGES[platform.result]:
-            otto.info("Installing packages for %s..." % platform.result)
-            to_install = ' '.join(PACKAGES[platform.result])
-            otto.shell("sudo apt-get install -y %s" % to_install)
-
-        # Install pip packages
-        if PIP_PACKAGES:
-            otto.info("Installing packages from pip..." % platform.result)
-            to_install = ' '.join(PIP_PACKAGES)
-            otto.shell("sudo pip install %s" % to_install)
+        _packages(platform.result)
+        _packages('python')
 
         # Configure everything
         with otto.ChangePath():
